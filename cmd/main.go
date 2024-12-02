af2a1346884424312fe9b1b4b6d5f6f8ef6cb67e
mraiyuu <munenedenis618@gmail.com>
2024-12-02 07:35:09 +0300

    refactor database connection

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"database/sql"
 	"github.com/go-sql-driver/mysql"
 	"github.com/mraiyuu/ecom/cmd/api"
 	"github.com/mraiyuu/ecom/cmd/config"
@@ -25,8 +26,18 @@ func main() {
 		log.Fatal(err)
 	}
 
+	initStorage(db)
+
 	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("DB succesfylly connected!")
 }
